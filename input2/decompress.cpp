5ce1c4504db9a3bd3377bc1b94fde6c158039580
ichn-hu <zfhu16@fudan.edu.cn>
2017-12-24 17:34:40 +0800

    Shrinking file 2 14512 = 12312 + 2200

--- input2/decompress.cpp
+++ input2/decompress.cpp
@@ -1,110 +1,80 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int N=1e6;
-int b_s,cur;
 unsigned char b[N],B[N];
-int S,I,A;
-
-typedef unsigned long long ull;
-ull C=(1<<24)-1,Q=1<<22,H=Q*2,T=Q*3,k,v;
-
-const int MAXO=4;
-struct Model{
+typedef unsigned long long U;
+U C=(1<<24)-1,Q=1<<22,H=Q*2,T=Q*3,k,v,h=C,o,V,Z,X,Y;
+int S,I,A,R,e[257],i,d,c,E;
+struct D{
     map<int,int> f;int c;
-    Model(){c=f[256]=1;}
+    D(){c=f[256]=1;}
     int u(int t){++c,++f[t];}
 };
-map<ull,Model> f[MAXO + 1];
-set<int> e;
-bool get_char(int &c,ull r,ull l,int&d,ull&tot,ull&o,ull&h) {    
-    if(d == -1) {
-        tot=257;
-        v=(l * tot - 1) / r;
-        c=o=v;
-        h=o + 1;
-        return false;
-    }
-
-    tot=0;
-    Model*m=&f[d][k];
-    for (int i=0;i < 257;++i)
-        if(!e.count(i))
-            tot += m->f[i];
-    v=(l * tot - 1) / r;
-
-    tot=0;
-    c=-1;
-    for (int i=0;i < 256;++i)
-        if(!e.count(i)) {
-            int t=m->f[i];
-            tot += t;
-            if(t != 0) e.insert(i);
-            if(tot > v && c == -1) {
-                o=tot - t;
-                h=tot;
-                c=i;
-                v=C;
-            }
-        }
-    if(c == -1) {
-        c=256;
-        o=tot;
-        h=tot=tot + m->f[256];
-        --d;
-        k >>= 8;
-        return true;
-    }
-    tot += m->f[256];
-    return false;
-}
-main() {
-    S=fread(B,1,N,fopen("c","rb"));
-    A=0x80;
-
-    cur=-1;
-    f[0][0]=Model();
-
-    ull h=C;
-    ull o=0;
-    ull V=0,_tot,_o,_h;
-    int d,c;
-    bool escape;
-    V = B[0] << 16 | B[1] << 8 | B[2];
+map<U,D> f[5];
+main(){
+    S=fread(B,1,N,fopen("c","rb"));A=128;R=-1;
+    f[0][0]=D();
+    V = *B << 16 | B[1] << 8 | B[2];
     I = 3;
     for (;;) {
-        e.clear();
+        fill(e,e+257,0);
         k=0;
-        if(!~cur)d=-1;else for (d=0;cur - d >= 0 && d<MAXO;++d) {
-            k=k << 8 | b[cur - d];
-            if(f[d+1].count(k) == 0) {
+        if(!~R)d=-1;else for(d=0;R-d+1&&d<4;++d) {
+            k=k << 8 | b[R - d];
+            if(!f[d+1].count(k)) {
                 k >>= 8;
                 break;
             }
         }
         do {
-            ull range=h - o + 1;
-            escape=get_char(c,range,V - o + 1,d,_tot,_o,_h);
-            h=o + (range * _h) / _tot - 1;
-            o=o + (range * _o) / _tot;
+            U r=h - o + 1,l=V - o + 1;
+            E=0;
+            if(!~d) {
+                Z=257;
+                c=X=v=(l * Z - 1) / r;
+                Y=X + 1;
+            } else {
+                Z=0;
+                D*m=&f[d][k];
+                for (i=257;~--i;)Z += m->f[i]*!e[i];
+                v=(l * Z - 1) / r;
+
+                Z=0;
+                c=256;
+                for (i=0;i<256;++i)
+                    if(!e[i]) {
+                        int t=m->f[i];
+                        e[i]=!!t;
+                        v<(Z+=t) ? 
+                            X=Z - t,
+                            Y=Z,
+                            c=i,
+                            v=C:0;
+                    }
+                ++Z;
+                c>>8?X=Z-1,Y=Z,--d,k>>=8,E=1:0;
+            }
+            h=o + r * Y / Z - 1;
+            o+=r * X / Z;
             V:
                 if(o>=H)
                     V-=H,o-=H,h-=H; 
                     else if(o>=Q&&h<T)V-=Q,o-=Q,h-=Q;
                 else if(h>=H)goto E;
                 o*=2;h=h*2+1;
-                V=V*2+(bool)(A&B[I]);
-                A/=2;A=!A?I+=I<S?1:B[I]=0,0x80:A;
+                V=V*2+!!(A&B[I]);
+                A/=2;A=!A?I+=I<S?1:B[I]=0,128:A;
             goto V;E:;
-        } while(escape);
-        if(c == 256) break;
-        for (int i=max(d,0);i<=MAXO;++i) {
-            if(!f[i].count(k))f[i][k]=Model();
+        } while(E);
+        if(c>>8) break;
+        for (i=d+!~d;i<5;++i) {
+            if(!f[i].count(k))f[i][k]=D();
             f[i][k].u(c);
-            if(cur-i+1)
-                k=k<<8|b[cur-i];
+            if(R-i+1)
+                k=k<<8|b[R-i];
             else break;
         }
-        b[++cur]=c;
+        b[++R]=c;
         cout<<bitset<8>(c);
     }
 }
