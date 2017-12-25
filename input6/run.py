78f295e30b16d5c5ba1f067a9067ce096ca15c6b
ichn-hu <zfhu16@fudan.edu.cn>
2017-12-25 17:00:14 +0800

    Shrinking file 6 77

--- input6/run.py
+++ input6/run.py
@@ -1,6 +1,7 @@
 import sys, os, time
 os.system("g++ -O2 solve.cpp")
 os.system("a.exe > result.temp")
+os.system("del a.exe")
 compressed_size = len(open("solve.cpp", "rb").read())
 print("Compressed size = {0} bytes".format(compressed_size))
 if open("result.temp", "r").read() != open("input6.txt", "r").read():
--- input6/solve.cpp
+++ input6/solve.cpp
@@ -1,2 +1,2 @@
 #include<cstdio>
-main(){for(int i;i<49152;)printf("0110"[i/12>>i++%12&3]);}
+main(){for(int i;i<49152;)putchar("0110"[i/12>>i++%12&3]);}
