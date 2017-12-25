0a6973a8b0b70d35b4ef39d3b093fdb05b75cd15
ichn-hu <zfhu16@fudan.edu.cn>
2017-12-25 16:17:54 +0800

    Shrinking file 6 121

--- input6/solve.cpp
+++ input6/solve.cpp
@@ -1,14 +1,6 @@
-#include <cstdio>
-#include <cstring>
-char s[5000][13] = {"000000000000"};
+#include<cstdio>
+int i, j;
 int main() {
-	for (int i = 0; i < 12;) {
-		for (int j = 0; j < (1<<i); ++j)
-			memcpy(s[(1<<(i+1)) - j - 1], s[j], 12);
-		for (int j = 1<<i++; j < (1<<i); ++j)
-			s[j][i-1] = '1';
-	}
-	for (int i = 0; i < 4096; ++i)
-		printf("%s", s[i]);
-	return 0;
+    for(; i<4096; i++)
+        for(j = 13; --j; putchar((i>>1^i)>>12-j&1^48));
 }
