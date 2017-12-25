861d7ba5c4999181c7f3f05a6f21b28b0130aa8d
ichn-hu <zfhu16@fudan.edu.cn>
2017-12-25 16:18:09 +0800

    Shrinking file 6 92

--- input6/solve.cpp
+++ input6/solve.cpp
@@ -1,6 +1,2 @@
 #include<cstdio>
-int i, j;
-int main() {
-    for(; i<4096; i++)
-        for(j = 13; --j; putchar((i>>1^i)>>12-j&1^48));
-}
+int i,j;main(){for(;i<4096;i++)for(j=13;--j;putchar((i>>1^i)>>12-j&1^48));}
