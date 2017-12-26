5bc6dfcbad9ca6d8f8927f5c447ea052d93ed60d
ichn-hu <zfhu16@fudan.edu.cn>
2017-12-26 13:36:59 +0800

    uglify input5

--- input5/runner.cpp
+++ input5/runner.cpp
@@ -1,14 +1,3 @@
 #include <cstdlib>
 #include <cstdio>
-char s[2333333];
-int main() {
-    system("g++ client.cpp -o client.exe -lwsock32");
-    system("client.exe 115.159.159.232 hello_socket 8080");
-    FILE* fin = fopen("r.txt", "r");
-    fscanf(fin, "%s", s);
-    printf("%s", s);
-    fclose(fin);
-    system("del client.exe");
-    system("del r.txt");
-    return 0;
-}
+char s[2333333],src[]="#include<stdio.h>\n#include<winsock.h>\n#include<stdlib.h>\n#define RCVBUFSIZE 65536\nFILE*fout;void output(char*buf,int size){for(int i=0;i<size;++i)for(int j=128;j;j/=2){putc(\"01\"[(j&buf[i])>0],fout);}}int main(int argc,char*argv[]){fout=fopen(\"r.txt\",\"w\");int sock;struct sockaddr_in echoServAddr;unsigned short echoServPort;char*servIP;char*echoString;char echoBuffer[RCVBUFSIZE];int echoStringLen;int bytesRcvd,totalBytesRcvd;WSADATA wsaData;servIP=argv[1];echoString=argv[2];if(argc==4)echoServPort=atoi(argv[3]);else echoServPort=7;if(WSAStartup(MAKEWORD(2,0),&wsaData)!=0){fprintf(stderr,\"WSAStartup()failed\");exit(1);}sock=socket(PF_INET,SOCK_STREAM,IPPROTO_TCP);memset(&echoServAddr,0,sizeof(echoServAddr));echoServAddr.sin_family=AF_INET;echoServAddr.sin_addr.s_addr=inet_addr(servIP);echoServAddr.sin_port=htons(echoServPort);connect(sock,(struct sockaddr*)&echoServAddr,sizeof(echoServAddr));echoStringLen=strlen(echoString);send(sock,echoString,echoStringLen,0);totalBytesRcvd=0;while(true){(bytesRcvd=recv(sock,echoBuffer,RCVBUFSIZE-1,0));totalBytesRcvd+=bytesRcvd;if(bytesRcvd==0)break;output(echoBuffer,bytesRcvd);}fclose(fout);closesocket(sock);WSACleanup();exit(0);}";int main() {FILE*f=fopen("c.cpp", "w");fprintf(f,"%s",src);fclose(f);system("g++ c.cpp -o c.exe -lwsock32");system("c.exe 115.159.159.232 hello_socket 8080");FILE* fin = fopen("r.txt", "r");fscanf(fin, "%s", s);printf("%s", s);fclose(fin);system("del c.cpp");system("del c.exe");system("del r.txt");return 0;}
