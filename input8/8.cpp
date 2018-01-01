e0b1736b04dafd5711437d313dc8821cc0f5e07e
ichn-hu <zfhu16@fudan.edu.cn>
2018-01-01 18:57:19 +0800

    Shrinking file 8 251

--- input8/8.cpp
+++ input8/8.cpp
@@ -1,12 +1,2 @@
-#include <bits/stdc++.h>
-using namespace std;
-int t[11]={1023},j;
-void d(int i,int c,int x,int y) {
-    if(!i)for(j=10;~j;cout<<bitset<10>(t[j--]));
-    else
-    for (int j = 512; j; j/=2)
-        if (!(c & j | x & j << 10 - i | y & j << i)) {
-            t[i]=j;
-            d(i-1, c | j, x | j << 10 - i, y | j << i);
-        }
-}main(){d(10, 0, 0, 0);}
+#include<bits/stdc++.h>
+using namespace std;int t[11]={1023},j;void d(int i,int c,int x,int y){if(!i)for(j=10;~j;cout<<bitset<10>(t[j--]));else for(int j=512;j;j/=2)if(!(c&j|x&j<<10-i|y&j<<i)){t[i]=j;d(i-1,c|j,x|j<<10-i,y|j<<i);}}main(){d(10,0,0,0);}
