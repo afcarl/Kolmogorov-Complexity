9e63940e54ea6bfd49c169b50fe21687134bf8f5
ichn-hu <zfhu16@fudan.edu.cn>
2018-01-01 17:45:06 +0800

    Shrinking file 8 439

--- input8/8.cpp
+++ input8/8.cpp
@@ -7,11 +7,7 @@ void dfs(int i, int c, int x, int y) {
     else
     for (int j = 9; ~j; --j)
         if (!((c & (1 << j)) | (x & (1 << (10 + j - i))) | (y & (1 << (i + j))))) {
-            t[i] = 1<<j;
+            t[i]=1<<j;
             dfs(i-1, c | (1 << j), x | (1 << (10 + j - i)), y | (1 << (i + j)));
         }
-}
-int main() {
-    dfs(10, 0, 0, 0);
-    return 0;
-}
+}main(){dfs(10, 0, 0, 0);}
