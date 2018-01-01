612642471c42f31f03b543ed2ca3ab6fa28338bb
ichn-hu <zfhu16@fudan.edu.cn>
2018-01-01 16:47:08 +0800

    Shrinking file 9 149

--- input9/9.cpp
+++ input9/9.cpp
@@ -1,6 +1,2 @@
 #include <bits/stdc++.h>
-using namespace std;
-main(){
-	for(unsigned a=16807,c=(unsigned)1<<31,x=1508303647,y,i=10000;i--;y=a*x,cout<<bitset<32>(x = (y + y / c % 2) % c));
-	return 0;
-}
+main(){for(unsigned a=16807,c=(unsigned)1<<31,x=1508303647,y,i=10000;i--;y=a*x,std::cout<<std::bitset<32>(x=(y+y/c%2)%c));}
