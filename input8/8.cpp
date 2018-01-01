911cc49adf6a2e57758c76b01d846eac2b6e30ea
ichn-hu <zfhu16@fudan.edu.cn>
2018-01-01 17:29:43 +0800

    Shrinking file 8 555

--- input8/8.cpp
+++ input8/8.cpp
@@ -1,2 +1,21 @@
-#include<bits/stdc++.h>
-int x[11];int t(int c,int l,int r,int i){if(i>9)for(i=0;i<11;++i)std::cout<<std::bitset<10>(x[i]);else for(int p=1023&~(c|l|r),q;q=p&-p,p;p-=q)x[i]=1<<(9-(int)log2(q)),t(c|q,(l|q)*2,(r|q)/2,i+1);}main(){x[10]=1023;t(0,0,0,0);}
+#include <bits/stdc++.h>
+using namespace std;
+int t[11];
+void dfs(int i, int c, int x, int y) {
+    if (i > 9) {
+        for (int j = 0; j < 11; ++j)
+            cout << bitset<10>(t[j]);
+        return ;
+    }
+    for (int j = 0; j < 10; ++j) {
+        if (!(c & (1 << j)) && !(x & (1 << (10 + j - i))) && !(y & (1 << (i + j)))) {
+            t[i] = 1<<10-j-1;
+            dfs(i + 1, c | (1 << j), x | (1 << (10 + j - i)), y | (1 << (i + j)));
+        }
+    }
+}
+int main() {
+    t[10] = (1<<10)-1;
+    dfs(0, 0, 0, 0);
+    return 0;
+}
