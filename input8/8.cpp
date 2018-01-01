a573ed9ee02316ec171a4e55cf80f58f980ac50a
ichn-hu <zfhu16@fudan.edu.cn>
2018-01-01 18:54:57 +0800

    Shrinking file 8 389

--- input8/8.cpp
+++ input8/8.cpp
@@ -1,13 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 int t[11]={1023},j;
-void dfs(int i, int c, int x, int y) {
-    if (!i)
-        for (j=10;~j;cout<<bitset<10>(t[j--]));
+void dfs(int i,int c,int x,int y) {
+    if(!i)for(j=10;~j;cout<<bitset<10>(t[j--]));
     else
-    for (int j = 9; ~j; --j)
-        if (!((c & (1 << j)) | (x & (1 << (10 + j - i))) | (y & (1 << (i + j))))) {
-            t[i]=1<<j;
-            dfs(i-1, c | (1 << j), x | (1 << (10 + j - i)), y | (1 << (i + j)));
+    for (int j = 512; j; j/=2)
+        if (!((c & j) | (x & (j << (10 - i))) | (y & (j << i)))) {
+            t[i]=j;
+            dfs(i-1, c | j, x | (j << (10 - i)), y | (j << i));
         }
 }main(){dfs(10, 0, 0, 0);}
